03e3d505f809b73f4c5cb8b865ae5a96915e5668
Kosukhin Daniil <daniil.kosuxin@gmail.com>
2024-10-15 17:19:30 +0300

    edit classes

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 import telebot
+from telebot import types
+
 
 bot = telebot.TeleBot('7505931690:AAEmlFx2JFy6lHf6t_80Bd_mqLlXatUqTjs')
 
 
 @bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, message)
+def main_menu(message):
 
+    inline_keyboard = types.InlineKeyboardMarkup()
+    inline_keyboard.add(
+        types.InlineKeyboardButton(
+            'Информация об авто', callback_data='car_info'))
 
-@bot.message_handler()
-def info(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет!')
-    elif message.text.lower() == 'id':
-        bot.reply_to(message, 'Не надо тебе этого')
+    inline_keyboard.add(
+        types.InlineKeyboardButton(
+            'Список комплектующих', callback_data='car_info'))
+
+    bot.send_message(
+        message.chat.id, '---Здесь должен быть приветственный текст---',
+        reply_markup=inline_keyboard)
 
 
 bot.polling(none_stop=True)
-
-
-
-
-
-# def start(m, res=False):
-#     bot.send_message(m.chat.id, 'Вы вошли телеграм бот второй квартиры')
-
-
-# @bot.message_handler(content_types=["text"])
-# def handle_text(message):
-#     bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
-
-
-# bot.polling(none_stop=True, interval=0)
